15b9568ece08685f771cbfda344588b523145b43
Faizan <faizank789@gmail.com>
2022-07-23 09:28:35 +0530

    Pipeline code

---
+++ code.groovy
@@ -0,0 +1,13 @@
+@Library('jenkins_lib') _
+pipeline {
+    agent any
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Working on process..'
+                test('faizan')
+            }
+        }
+    }
+}
+
