e0c575082b7aed8a7a4c90c5e9f288eeafc0f76c
Faizan <faizank789@gmail.com>
2022-07-23 12:58:52 +0530

    fixed

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,3 @@
-class Example {
-   static def test(name) {
-      println(name)
-      println("This is how methods work in groovy");
-      println("This is an example of a simple method");
-   } 
+def call(name) {
+   echo "welcome ${name} !"
+}
